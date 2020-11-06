521ba576e7290ce65a86cbe01ce2faccb3af683c
biscatcho10 <karimbiscatcho@gmail.com>
2020-11-06 17:39:24 +0200

    initial commit

---
+++ routes/dashboard.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('/', 'DashboardController@index')->name('dashboard.index');
+});
